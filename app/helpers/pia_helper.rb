6529dca3a1153298392f0a3734b896b428c3991e
mtuduri <matias_tuduri@hotmail.com>
2018-01-21 16:39:08 -0300

    desing

--- app/helpers/pia_helper.rb
+++ app/helpers/pia_helper.rb
@@ -1,11 +1,30 @@
 module PiaHelper
 
-  def nested_groups(groups)
-    s = content_tag(:ul) do
-      groups.map do |group, sub_groups|
-        content_tag(:li, "#{group.pid} - #{group.titulo}") +  nested_groups(sub_groups)
-      end.join.html_safe
+  def nested_pias(groups)
+    result = content_tag(:div) do
+      	groups.map do |pia, children|
+
+      		header = content_tag :div, :class => "card-header", :id => "#{pia.pid}-title" do
+      			content_tag :h5, :class => "mb-0" do
+      				content_tag :button, :class => "btn btn-link", :"data-toggle" => "collapse", :"data-target"=> "##{pia.pid}", :"aria-expanded" =>(pia.ancestry ? "false" : "true"), :"aria-controls" =>"#{pia.pid}" do
+			      		"#{pia.pid} - #{pia.titulo}"
+			      end
+			    end
+			end
+			expanded = pia.ancestry ? "" : "show";
+			body = 
+			content_tag :div, :"aria-labelledby" => "#{pia.pid}-title", :"data-parent"=>(pia.ancestry ? "##{pia.ancestry}" : "#accordion"), :class => "collapse #{expanded}", :id => "#{pia.pid}" do
+			 	content_tag :div, :class => "card-body nested_pia" do
+      			  content_tag(:div, pia.informacion) +  nested_pias(children)
+				end
+			end
+
+	      	card = content_tag :div, :class => "card" do
+					(header + body).html_safe
+	      	end
+    	end.join.html_safe		
     end
+
   end
 
-end
+ end
