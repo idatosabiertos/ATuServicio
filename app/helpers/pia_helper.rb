eb496cbbff20e1e209f1b18464425a3379662360
mtuduri <matias_tuduri@hotmail.com>
2018-02-25 21:11:02 -0300

    arreglo jerarquia

--- app/helpers/pia_helper.rb
+++ app/helpers/pia_helper.rb
@@ -4,16 +4,30 @@ module PiaHelper
     result = content_tag(:div) do
       	groups.map do |pia, children|
 
+			is_first = pia.pid =='1'
+			expanded = is_first ? "in show" : "";
+			angle_icon= is_first ? "fa-angle-up" : "fa-angle-down"
+
+			link_icon = content_tag :i, :class => "pias-link-icon fa #{angle_icon}" do "" end
+
       		header = content_tag :div, :class => "card-header", :id => "#{pia.pid}-title" do
       			content_tag :h5, :class => "mb-0" do
-      				content_tag :button, :class => "btn btn-link", :"data-toggle" => "collapse", :"data-target"=> "##{pia.pid}", :"aria-expanded" =>(pia.ancestry ? "false" : "true"), :"aria-controls" =>"#{pia.pid}" do
-			      		"#{pia.pid} - #{pia.titulo}"
-			      end
+      				content_tag :a, 
+      				:class => "btn btn-link pias-link",
+      				  :"data-toggle" => "collapse",
+      				  :href => "##{pia.pid}",
+      				  :"aria-expanded" =>(is_first ? "true" : "false"),
+      				  :"aria-controls" =>"#{pia.pid}" do
+			      		((content_tag :span, "#{pia.pid} - #{pia.titulo}") + link_icon ).html_safe 
+			      		
+			      	end
 			    end
 			end
-			expanded = pia.ancestry ? "" : "show";
+			
 			body = 
-			content_tag :div, :"aria-labelledby" => "#{pia.pid}-title", :"data-parent"=>(pia.ancestry ? "##{pia.ancestry}" : "#accordion"), :class => "collapse #{expanded}", :id => "#{pia.pid}" do
+			content_tag :div,
+				:id => "#{pia.pid}",
+			    :class => "collapse #{expanded}" do
 			 	content_tag :div, :class => "card-body nested_pia" do
       			  content_tag(:div, pia.informacion) +  nested_pias(children)
 				end
