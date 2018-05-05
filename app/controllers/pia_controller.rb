5d004de61b260ff7426cddb3595bd894a20f43f6
mtuduri <matias_tuduri@hotmail.com>
2018-05-05 19:47:22 -0300

    faltaba considear categoria en autocomplete

--- app/controllers/pia_controller.rb
+++ app/controllers/pia_controller.rb
@@ -6,13 +6,25 @@ class PiaController < ApplicationController
     @title = 'Catálogo Prestaciones'
 
     if params[:category]
-      search = params[:category]
-      @pia = Pia.where('ancestry=? or pid =?', search, search).arrange
+      $category = params[:category]
+      @pia = Pia.where('pid LIKE ? OR pid = ? OR ancestry LIKE ? OR ancestry = ?', "#{$category}.%", $category, "#{$category}%", $category).arrange
     else
       @pia = Pia.all.arrange
     end
 
     @categories = Pia.all.where(ancestry: nil).order(:pid)
+  end
+
+  def autocomplete_pia_titulo
+    term = params[:term]
+    @pias = []
+    puts $cat
+    if $category 
+      @pias = Pia.where('(pid LIKE ? OR pid = ? OR ancestry LIKE ? OR ancestry = ?) AND lower(titulo) LIKE  lower(?)', "#{$category}.%", $category, "#{$category}%", $category, "%#{term}%").order(:titulo).all
+    else
+      @pias = Pia.where('lower(titulo) LIKE lower(?)', "%#{term}%").order(:titulo).all
+    end
+    render :json => @pias.map { |product| {:id => product.pid, :label => product.titulo, :value => product.titulo} }
   end
 
 end
